3141c8f34e18089ead4f473a6d23c599f2243377
KRochaS <karinerocha.s@hotmail.com>
2024-03-11 20:30:04 -0300

    feat: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
-import fastify from 'fastify'
+import fastify from 'fastify';
+import { knex } from './database';
+
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello world'
+app.get('/hello', async () => {
+    const tables = await knex('sqlite_schema').select('*');
+
+    return tables;
 })
 
 app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP server running on http://localhost:3333')
-  })
+    .listen({
+        port: 3333,
+    })
+    .then(() => {
+        console.log('HTTP server running on http://localhost:3333')
+    })

cbc669eedccfadf08fe9f10ebe3f7ae52cb2176c
KRochaS <karinerocha.s@hotmail.com>
2024-03-19 22:25:29 -0300

    feat: Sumary route

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,46 +1,54 @@
-import { FastifyInstance } from 'fastify';
-import { knex } from '../database';
-import { z } from 'zod';
-import {randomUUID} from 'node:crypto';
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { z } from "zod";
+import { randomUUID } from "node:crypto";
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get("/", async () => {
+    const transactions = await knex("transactions").select();
 
-    app.get('/', async () => {
-        const transactions = await knex('transactions').select();
+    return {
+      transactions,
+    };
+  });
 
-        return {
-            transactions,
-        }
-    })
+  app.get("/:id", async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = getTransactionParamsSchema.parse(request.params);
 
-    app.get('/:id', async (request) => {
-        const getTransactionParamsSchema = z.object({
-            id: z.string().uuid(),
-        })
-        const {id} = getTransactionParamsSchema.parse(request.params);
+    const transaction = await knex("transactions").where("id", id).first();
 
-        const transaction = await knex('transactions').where('id', id).first()
+    return { transaction };
+  });
 
-        return {transaction}
-    })
+  app.post("/", async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(["credit", "debit"]),
+    });
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
 
-    app.post('/', async (request, reply) => {
-       const createTransactionBodySchema = z.object({
-        title: z.string(),
-        amount: z.number(),
-        type: z.enum(['credit', 'debit'])
-       })
-        const {title, amount, type} = createTransactionBodySchema.parse(request.body);
-    
-       await knex('transactions')
-        .insert({
-            id: randomUUID(),
-            title,
-            amount: type === 'credit' ? amount : amount * -1,
-        })
+    await knex("transactions").insert({
+      id: randomUUID(),
+      title,
+      amount: type === "credit" ? amount : amount * -1,
+    });
 
-       return reply.status(201).send();
-    
-    
-    })
+    return reply.status(201).send();
+  });
+
+  app.get("/sumary", async () => {
+    const sumary = await knex("transactions")
+      .sum("amount", { as: "amount" })
+      .first();
+
+    return {
+      sumary,
+    };
+  });
 }
